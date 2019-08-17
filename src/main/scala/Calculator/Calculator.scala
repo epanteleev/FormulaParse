9722d2eae52a9d07fb7b57fe24dec9d7a6283fa0
minium2 <msl1998panteleev@yandex.ru>
2019-08-17 22:47:08 +0700

    up

--- src/main/scala/Calculator/Calculator.scala
+++ src/main/scala/Calculator/Calculator.scala
@@ -2,29 +2,59 @@ package Calculator
 
 import scala.util.parsing.combinator.RegexParsers
 
+sealed trait Expression
+
+trait Math extends Expression {
+  def eval: Double
+}
+
+case class Number(double: Double) extends Math{
+  override def eval: Double = double
+}
+case class Summ (a:Math, b: Math) extends Math{
+  override def eval: Double = a.eval + b.eval
+}
+case class Sub (a:Math, b: Math)extends Math{
+  override def eval: Double = a.eval - b.eval
+}
+case class Prod (a:Math, b: Math)extends Math{
+  override def eval: Double = a.eval * b.eval
+}
+case class Div(a:Math, b: Math)extends Math{
+  override def eval: Double = a.eval / b.eval
+}
+
+case class id (name: String) extends Math{
+  override def eval: Double = throw new Error("Calculator.id")
+}
 
 object Calculator extends RegexParsers {
-  def number: Parser[Double] = "-" ~> number ^^ { n => -n.toDouble} |
-    """\d+(\.\d*)?""".r ^^ { n => n.toDouble }
 
-  def factor: Parser[Double] = number | "(" ~> expr <~ ")"
+  def number: Parser[Math] = """-?\d+(\.\d*)?""".r ^^ { n => Number(n.toDouble) }
 
-  def term  : Parser[Double] = factor ~ rep( "*" ~ factor | "/" ~ factor) ^^ {
+  //def id: Parser[Expression] = "[a-zA-Z][a-zA-Z0-9_]*".r ^^ id
+
+  def factor: Parser[Math] = number | "(" ~> expr <~ ")"
+
+  def term  : Parser[Math] = factor ~ rep( ("*"  | "/") ~ factor) ^^ {
     case number ~ list => list.foldLeft(number) {
-      case (x, "*" ~ y) => x * y
-      case (x, "/" ~ y) => x / y
+      case (x, "*" ~ y) => Prod(x,y)
+      case (x, "/" ~ y) => Div(x,y)
     }
   }
 
-  def expr  : Parser[Double] = term ~ rep("+" ~ term | "-" ~ term) ^^ {
+  def expr  : Parser[Math] = term ~ rep("+" ~ term | "-" ~ term) ^^ {
     case number ~ list => list.foldLeft(number) {
-      case (x, "+" ~ y) => x + y
-      case (x, "-" ~ y) => x - y
+      case (x, "+" ~ y) => Summ(x, y)
+      case (x, "-" ~ y) => Sub(x, y)
     }
   }
 
   def apply(input: String): Double = parseAll(expr, input) match {
-    case Success(result, _) => result
+    case Success(result, i) =>
+      println(result)
+      // println(i)
+      result.eval
     case failure : NoSuccess => scala.sys.error(failure.msg)
   }
 }
--- src/main/scala/Calculator/Main.scala
+++ src/main/scala/Calculator/Main.scala
@@ -5,5 +5,5 @@ object Main extends App {
   println(Calculator("(1 + 4)*2 - 4.0"))
   println(Calculator("( 1 + 4 ) * 2 - 4.0"))
   println(Calculator("(-1 + 4 ) * 2 - 4.0"))
-
+  val map = Map( "sin" -> ((x : Double) => math.sin(x)))
 }
